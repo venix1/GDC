b92cf14f26998dd50fc55b20228984d1a13c3ea1
Iain Buclaw <ibuclaw@ubuntu.com>
2012-07-18 17:55:04 +0100

    Remove va_arg template in core.vararg.

--- gcc/d/d-codegen.h
+++ gcc/d/d-codegen.h
@@ -136,7 +136,6 @@ struct IRState : IRBase
     INTRINSIC_RNDTOL, INTRINSIC_SIN,
     INTRINSIC_SQRT,
 
-    INTRINSIC_STD_VA_ARG,
     INTRINSIC_C_VA_ARG,
     INTRINSIC_C_VA_START,
     INTRINSIC_count,
@@ -327,9 +326,8 @@ struct IRState : IRBase
   static Module *intrinsicCoreModule;
   static Module *mathModule;
   static Module *mathCoreModule;
-  static TemplateDeclaration *stdargTemplateDecl;
+  static TemplateDeclaration *cstdargTemplateDecl;
   static TemplateDeclaration *cstdargStartTemplateDecl;
-  static TemplateDeclaration *cstdargArgTemplateDecl;
 
   static void setBuiltinsModule (Module *mod)
   { IRState::builtinsModule = mod; }
@@ -350,14 +348,11 @@ struct IRState : IRBase
       IRState::mathModule = mod;
   }
 
-  static void setStdArg (TemplateDeclaration *td)
-  { IRState::stdargTemplateDecl = td; }
+  static void setCStdArg (TemplateDeclaration *td)
+  { IRState::cstdargTemplateDecl = td; }
 
   static void setCStdArgStart (TemplateDeclaration *td)
   { IRState::cstdargStartTemplateDecl = td; }
-
-  static void setCStdArgArg (TemplateDeclaration *td)
-  { IRState::cstdargArgTemplateDecl = td; }
 
   static bool maybeSetUpBuiltin (Declaration *decl);
 
