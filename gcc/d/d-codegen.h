e25fc68e3c0760ec841b807fcdfab71a5793005f
Iain Buclaw <ibuclaw@ubuntu.com>
2012-07-18 18:02:11 +0100

    Some more coding convention fixes... get started on code documentation for each function.

--- gcc/d/d-codegen.h
+++ gcc/d/d-codegen.h
@@ -476,7 +476,7 @@ struct ListMaker
 
   ListMaker (void)
     : head(NULL_TREE),
-      ptail_(& this->head)
+      ptail_(&this->head)
   { }
 
   ListMaker (tree *alt_head)
@@ -552,7 +552,7 @@ class AggLayout : public FieldVisitor
   AggLayout (AggregateDeclaration *ini_agg_decl, tree ini_agg_type)
     : FieldVisitor(ini_agg_decl),
       aggType_(ini_agg_type),
-      fieldList_(& TYPE_FIELDS(this->aggType_))
+      fieldList_(&TYPE_FIELDS(this->aggType_))
   { }
 
   void doFields (VarDeclarations *fields, AggregateDeclaration *agg);
--- gcc/d/d-todt.cc
+++ gcc/d/d-todt.cc
@@ -24,17 +24,17 @@
 dt_t **
 ComplexExp::toDt (dt_t **pdt)
 {
-  return dttree (pdt, toElem (& gen));
+  return dttree (pdt, toElem (&gen));
 }
 
 dt_t **
 IntegerExp::toDt (dt_t **pdt)
 {
-  return dttree (pdt, toElem (& gen));
+  return dttree (pdt, toElem (&gen));
 }
 
 dt_t **
 RealExp::toDt (dt_t **pdt)
 {
-  return dttree (pdt, toElem (& gen));
+  return dttree (pdt, toElem (&gen));
 }
